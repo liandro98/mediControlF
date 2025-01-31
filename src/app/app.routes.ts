277fc2e3884dc77cfde0e11e394ba405f3f928a8
liandro98 <miguelalejandroespinosa7@gmail.com>
2025-01-31 13:59:31 -0600

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PaginaBienvenidaComponent } from './components/pagina-bienvenida/pagina-bienvenida.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:PaginaBienvenidaComponent,
+        pathMatch:'full'
+    }
+];
---
+++ src/app/components/pagina-bienvenida/pagina-bienvenida.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BarraNavegacionComponent } from "../barra-navegacion/barra-navegacion.component";
+
+@Component({
+  selector: 'app-pagina-bienvenida',
+  standalone: true,
+  imports: [BarraNavegacionComponent],
+  templateUrl: './pagina-bienvenida.component.html',
+  styleUrl: './pagina-bienvenida.component.css'
+})
+export class PaginaBienvenidaComponent {
+
+}
