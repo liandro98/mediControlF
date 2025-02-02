60254b8d922b6563fafc700371752d5cfe48c3e5
liandro98 <miguelalejandroespinosa7@gmail.com>
2025-02-02 13:52:43 -0600

    configuracion de rutas y nuevos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,25 @@
 import { Routes } from '@angular/router';
 import { PaginaBienvenidaComponent } from './components/pagina-bienvenida/pagina-bienvenida.component';
+import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 export const routes: Routes = [
     {
         path:'',
         component:PaginaBienvenidaComponent,
         pathMatch:'full'
+    },
+    {
+        path:'principal',
+        component:PaginaPrincipalComponent
+    },
+    {
+        path:'login',
+        component:InicioSesionComponent
+    },
+    {
+        path:'registro',
+        component:RegistroComponent
     }
 ];
