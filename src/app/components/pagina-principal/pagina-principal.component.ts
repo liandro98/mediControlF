3d6a4ef89295dcdafea8b982a40d8ec949ae1100
liandro98 <miguelalejandroespinosa7@gmail.com>
2025-02-03 10:40:20 -0600

    Panel de control

--- src/app/components/pagina-principal/pagina-principal.component.ts
+++ src/app/components/pagina-principal/pagina-principal.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BarraNavegacionComponent } from "../barra-navegacion/barra-navegacion.component";
 
 @Component({
   selector: 'app-pagina-principal',
   standalone: true,
-  imports: [],
+  imports: [BarraNavegacionComponent],
   templateUrl: './pagina-principal.component.html',
   styleUrl: './pagina-principal.component.css'
 })
