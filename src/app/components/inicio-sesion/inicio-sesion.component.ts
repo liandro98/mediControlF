77c428b5944357c2b88e23eea80ffab4f98e4a5a
liandro98 <miguelalejandroespinosa7@gmail.com>
2025-02-07 17:23:16 -0600

    Login operativo

--- src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthServiceService } from '../../servicies/auth-service.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio-sesion',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.css'
 })
 export class InicioSesionComponent {
+  usuario: string = '';
+  clave: string = '';
+  errorMessage: string = '';
 
+  constructor(private authService: AuthServiceService, private router: Router){}
+
+  login() {
+    const credentials = {
+      usuario: this.usuario,
+      clave: this.clave
+    };
+  
+    this.authService.login(credentials).subscribe(
+      (response) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/principal']);
+      },
+      (error) => {
+        this.errorMessage = 'Credenciales incorrectas';
+      }
+    );
+  }
 }
---
+++ src/app/servicies/auth-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  register(user: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, user);
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('authToken', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+  }
+}
